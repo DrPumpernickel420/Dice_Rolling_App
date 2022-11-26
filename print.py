dc233c64d70b3939b88a391ef9a373b94d8ef0e6
Dr.Pumpernickel <114339487+DrPumpernickel420@users.noreply.github.com>
2022-11-26 12:04:02 +0100

    Update print.py

--- print.py
+++ print.py
@@ -7,8 +7,12 @@ def return_dices(rolls):
         ["┌─────────┐","│  ●   ●  │","│    ●    │","│  ●   ●  │","└─────────┘"],
         ["┌─────────┐","│  ●   ●  │","│  ●   ●  │","│  ●   ●  │","└─────────┘"],
     ]
-    result = []
-    dices = [dice[x-1] for x in rolls]
-    for x in range(0,5):
-        result.append("   ".join([dice[x] for dice in dices]))
-    return "\n".join(result)
+    list_of_dices = [rolls[x:x+5] for x in range(0,len(rolls),5)]
+    final_result = []
+    for x in list_of_dices:
+        result = []
+        dices = [dice[i-1] for i in x]
+        for x in range(0,5):
+            result.append("   ".join([dice[x] for dice in dices]))
+        final_result.append("\n".join(result))
+    return "\n\n\n".join(final_result)
